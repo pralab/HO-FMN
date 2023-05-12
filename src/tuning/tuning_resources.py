1803e02270cd626310afcab6557f8dc4e6d4be66
lucascionis <lucascionis@tiscali.it>
2023-05-12 19:17:26 +0200

    Created src/tuning
    - tune_fmn.py: tune FMN attack

---
+++ src/tuning/tuning_resources.py
@@ -0,0 +1,24 @@
+"""
+Define here your Ray resources - for hyperparams tuning
+
+e.g. cpu x trial:
+
+{
+    'cpu': n,
+    'cpu': n,
+    ...
+}
+
+or cpu and gpu x trial
+
+{
+    'gpu': ...,
+    'cpu': ...,
+    ...
+}
+"""
+
+TUNING_RES = {
+    'cpu': 6,
+    'cpu': 6
+}
